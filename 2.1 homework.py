5acc93b62b7acdb3833f5947339d68567d39d314
natxs27 <126962695+natxs27@users.noreply.github.com>
2023-03-11 17:01:42 +0100

    Add files via upload

---
+++ 2.1 homework.py
@@ -0,0 +1,5 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=str(int(8667230*6))
+>>> print(a.count('0'))
+3
