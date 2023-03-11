1304a5251fdc18b951b23a3c571c84551d2d1017
natxs27 <126962695+natxs27@users.noreply.github.com>
2023-03-11 17:20:31 +0100

    Add files via upload

---
+++ 2.3.homework.py
@@ -0,0 +1,4 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(sum([(x) ** 2 for x in range(1, 2022, 2)]))
+1377818771
