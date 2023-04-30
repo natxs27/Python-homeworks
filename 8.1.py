b1c2d66daff643f1771bbfcd289cc068785c1de0
natxs27 <126962695+natxs27@users.noreply.github.com>
2023-04-30 21:55:33 +0200

    Add files via upload

---
+++ 8.1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#a)
+a = np.linspace(0, 1, num=11, dtype=float)
+print(a)
+
+#b)
+b = np.zeros((5,6), dtype='b')
+print(b)
+
+#c
+x = np.array([0,1,2,3,4,5,6,7,8], dtype=complex)
+c = 4 ** x
+print(c)
